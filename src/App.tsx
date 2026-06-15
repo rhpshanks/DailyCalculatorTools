3d7a4c3e26f310a8701e52e8ea2d3b76c89333da
Hashaam <4generalprps@gmail.com>
2026-06-15 23:24:49 +0500

    feat: enable Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@
  */
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import ToolPage from './pages/ToolPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path=":slug" element={<ToolPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path=":slug" element={<ToolPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
